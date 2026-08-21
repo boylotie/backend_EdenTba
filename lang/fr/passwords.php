49b54dd1f3f8cbd453165c0bed99c93da4a78fbc
boylotie <156178797+boylotie@users.noreply.github.com>
2026-08-21 16:41:32 +0000

    dd

---
+++ lang/fr/passwords.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de langue de réinitialisation de mot de passe
+    |--------------------------------------------------------------------------
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous vous avons envoyé par e-mail le lien de réinitialisation de votre mot de passe.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation du mot de passe est invalide.',
+    'user' => "Nous n'avons trouvé aucun utilisateur avec cette adresse e-mail.",
+
+];
