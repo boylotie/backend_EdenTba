829ce69d4f56829df9d73cbeb42afb690421ed33
boylotie <156178797+boylotie@users.noreply.github.com>
2026-08-21 10:36:21 +0000

    f

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-use function fake;
-
 /**
  * @extends Factory<User>
  */
@@ -27,8 +25,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
